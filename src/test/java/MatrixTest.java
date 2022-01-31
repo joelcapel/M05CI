a414b8571f739d5d385b94551c0a5597d4201b37
joelcapel <jcapel@elpuig.xeill.net>
2022-01-31 19:40:29 +0100

    completed

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class MatrixTest {
 
     @ParameterizedTest
